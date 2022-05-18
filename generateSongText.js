95ba4dbd0c1b390861967da2b41f348ef6ab8578
viskhan <viskhan.rkb@gmail.com>
2022-05-18 16:40:48 +0300

    made 99-bottle

---
+++ generateSongText.js
@@ -0,0 +1,27 @@
+function generateSongText () {
+    for (let i = 99; i > 0; i--) {
+        print(i);
+    }
+};
+
+function print (countBottle) {
+    console.log(`${countBottle} ${generateSingleStanza(countBottle)} пива на стене, ${countBottle} ${generateSingleStanza(countBottle)} пива!
+    Возьми одну, пусти по кругу, ${--countBottle} ${generateSingleStanza(countBottle)} пива на стене!`)
+};
+
+function generateSingleStanza (countBottle) {
+    if (countBottle > 1 && countBottle < 5) {
+        return "бутылки";
+    }
+    else if (countBottle === 1 || countBottle % 10 === 1) {
+        return "бутылка";
+    }
+    else if ((countBottle > 4 && countBottle < 21) || countBottle % 10 === 0 || countBottle % 10 === 2 || countBottle % 10 === 3 || countBottle % 10 === 4) {
+        return "бутылок";
+    }
+    else {
+        return "бутылки";
+    }
+};
+
+generateSongText();
